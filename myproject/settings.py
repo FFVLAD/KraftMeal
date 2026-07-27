e24b71afdd78d8666f90fbbafd05a57a9f5df4c1
Vlad <you@example.com>
2026-07-27 15:36:03 +0200

    Add STATIC_ROOT and Render CSRF origins

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -78,6 +79,7 @@ DATABASES = {
 }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
@@ -104,6 +106,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "shop" / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -113,6 +117,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://192.168.100.142',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'https://*.onrender.com',
 ]
 
 CSRF_COOKIE_SECURE = False
